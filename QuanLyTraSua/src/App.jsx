37a2f127c6de506f4674337440749489e0a2e1f4
minh_Coder_sudo <dangquangminh232005@gmail.com>
2026-03-10 09:52:23 +0700

    fix bug

--- QuanLyTraSua/src/App.jsx
+++ QuanLyTraSua/src/App.jsx
@@ -1,35 +1,27 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './Component/page/Login';
-import Register from './Component/page/Register';
-import Profile from './Component/page/Profile';
+import Login from './Component/page/Login.jsx';
+import Register from './Component/page/Register.jsx';
+import Profile from './Component/page/Profile.jsx';
 import './App.css';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Mặc định vào sẽ chuyển đến trang login */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
 import Footer from './Component/Footer';
 import Header from './Component/Header';
-import banner from './assets/logo.png';
-
-export default function App() {
+function App() {
     return (
         <div className="min-h-screen bg-black">
             <Header />
-            <div className="pt-20"></div>
+            <Router className="pt-20">
+                <Routes>
+                    {/* Mặc định vào sẽ chuyển đến trang login */}
+                    <Route path="/" element={<Navigate to="/login" />} />
+
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </Router>
             <Footer />
         </div>
     );
 }
+
+export default App;
