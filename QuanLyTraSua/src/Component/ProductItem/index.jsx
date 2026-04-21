07a755d76476470a1a12b3b82720661923e7e47b
minh_Coder_sudo <dangquangminh232005@gmail.com>
2026-04-21 12:39:35 +0700

    sửa lại phần card của product

--- QuanLyTraSua/src/Component/ProductItem/index.jsx
+++ QuanLyTraSua/src/Component/ProductItem/index.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 function ProductItem({ name, basePrice = 0, image, description, onViewDetail }) {
     return (
         <div className="product-item">
-            <div>
-                <h3 className="w-64 text-orange-500 font-weight-600 text text-xl">{name}</h3>
-                <p className="w-64">{description}</p>
-                <p>{basePrice.toLocaleString()} VND</p>
+            <div className="product-info">
+                <h3 className="product-name">{name}</h3>
+                <p className="product-description">{description}</p>
+                <p className="product-price">{basePrice.toLocaleString()} VND</p>
                 <button className="product-button" onClick={onViewDetail}>
                     Chi tiết
                 </button>
             </div>
 
-            <img src={`http://localhost:5000${image}`} alt={name} width="120" className="product-image" />
+            <img src={`http://localhost:5000${image}`} alt={name} className="product-image" />
         </div>
     );
 }
